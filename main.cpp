8522aaaa702b630dcf1c48d891d56b0d1456d8db
travmygit <travmymail@gmail.com>
2021-03-10 00:12:58 +0800

    add eigen

--- main.cpp
+++ main.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <Eigen/Dense>
 #include "maths.h"
 #include "graphics.h"
 #include "rasterizer.h"
@@ -12,6 +13,14 @@ int main()
 	fff::IntPoint V3(80, 40);
 	r.DrawTriangle(V1, V2, V3, fff::Color::Blue);
     r.Serialize("output2.png");
+
+	
+	Eigen::MatrixXd m(2, 2);
+	m(0, 0) = 3;
+	m(1, 0) = 2.5;
+	m(0, 1) = -1;
+	m(1, 1) = m(1, 0) + m(0, 1);
+	std::cout << m << std::endl;
 
     return 0;
 }
